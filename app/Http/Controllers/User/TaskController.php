4ccd65027e7fd74fa4098119660ff2f2ca01526b
PovilasKorop <povilask007@gmail.com>
2025-06-03 09:16:00 +0300

    All files for separate admin/user areas

---
+++ app/Http/Controllers/User/TaskController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\User;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+ 
+class TaskController extends Controller
+{
+    public function index(): View
+    {
+        $tasks = auth()->user()->tasks;
+ 
+        return view('user.tasks.index', compact('tasks'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin;
+use App\Http\Controllers\User;
+use App\Http\Middleware\IsAdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,6 +15,19 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::prefix('admin')
+        ->name('admin.')
+        ->middleware(IsAdminMiddleware::class)
+        ->group(function () {
+            Route::resource('tasks', Admin\TaskController::class);
+        });
+
+    Route::prefix('user')
+        ->name('user.')
+        ->group(function () {
+            Route::resource('tasks', User\TaskController::class);
+        });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
